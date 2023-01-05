7ae1a1180e9c0a5922bed5d00ed1a1182d9bbd85
Ben <benawhiting@gmail.com>
2023-01-05 14:51:32 -0800

    Create schema for Comment model

---
+++ server/models/comment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
+
+const CommentSchema = new Schema({
+  content: { type: String, required: true, maxLength: 500 },
+  date_posted: {type: Date, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+CommentSchema.virtual('url').get(function() {
+  return `/comments/${this._id}`;
+});
+
+CommentSchema.virtual('date_formatted').get(function () {
+  return DateTime.fromJSDate(this.date_posted).toLocaleString(DateTime.DATE_MED);
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
