57ef411d5ddc45e64273bc88a089bf627d6f27c8
Ben <benawhiting@gmail.com>
2023-01-07 12:15:40 -0800

    Display additional post info on blog post previews

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { DateTime } from 'luxon';
 
 import '../stylesheets/App.css';
 
@@ -19,6 +20,11 @@ const App = () => {
       {posts.map((post) => 
         <div key={post._id} className='post-card'>
           <div className="card-title">{post.title}</div>
+          <div className="card-author">{post.author}</div>
+          <div className="card-date">
+            {DateTime.fromISO(post.date_posted).toLocaleString(DateTime.DATE_MED)}
+          </div>
+          <div className="card-content">{post.content}</div>
         </div>
       )}
     </div>
