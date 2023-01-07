2a68dada8ea4d5377610155a7c8084add8452929
Ben <benawhiting@gmail.com>
2023-01-07 12:54:19 -0800

    Pull blog post author username from Users array by matching with Post author ID

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,22 +5,38 @@ import '../stylesheets/App.css';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
 
+  // Fetches posts from server and stores them in state
   useEffect(() => {
     fetch('http://localhost:3001/posts')
     .then((response) => response.json())
     .then((data) => {
       setPosts(data.post_list);
     });
-    console.log(posts);
   }, []);
+
+  // Fetches users from server and stores them in state
+  useEffect(() => {
+    fetch('http://localhost:3001/users')
+    .then((response) => response.json())
+    .then((data) => {
+      setUsers(data.user_list);
+    });
+  }, []);
+
+  const displayAuthor = (author) => {
+    const result = users.find(user => user._id === author);
+
+    return result.username;
+  }
 
   return (
     <div className='app'>
       {posts.map((post) => 
         <div key={post._id} className='post-card'>
           <div className="card-title">{post.title}</div>
-          <div className="card-author">{post.author}</div>
+          <div className="card-author">{displayAuthor(post.author)}</div>
           <div className="card-date">
             {DateTime.fromISO(post.date_posted).toLocaleString(DateTime.DATE_MED)}
           </div>
