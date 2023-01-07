e77e1cf40406bca34dbb827cd68d3c90f0bc6ad2
Ben <benawhiting@gmail.com>
2023-01-07 12:04:13 -0800

    Provide basic styling for blog post previews

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import '../stylesheets/App.css';
+
 const App = () => {
   const [posts, setPosts] = useState([]);
 
@@ -13,9 +15,11 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <div className='app'>
       {posts.map((post) => 
-        <div key={post._id}>{post.content}</div>
+        <div key={post._id} className='post-card'>
+          <div className="card-title">{post.title}</div>
+        </div>
       )}
     </div>
   );
