4989a40b421fbda72d98206cca11f01e1a6d69c7
Ben <benawhiting@gmail.com>
2023-01-09 11:41:30 -0800

    Create skeleton layout for blog post detail page

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import Nav from './Nav';
+import Footer from './Footer';
+import '../stylesheets/Post.css';
+
+// FLESH THIS COMPONENT OUT TO SHOW POST DETAILS
 const Post = () => {
   return (
-    <div>Hello, World!</div>
+    <div className='blog-post'>
+      <Nav />
+      <div className='post'>Hello, World!</div>
+      <Footer />
+    </div>
   );
 };
 
