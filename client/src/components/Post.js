ae35d9768b48fd7f04f6a021d1b3433794c6636b
Ben <benawhiting@gmail.com>
2023-01-09 12:41:43 -0800

    Pass state through React Router Link using useLocation

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import { DateTime } from 'luxon';
 
 import Nav from './Nav';
 import Footer from './Footer';
@@ -7,11 +8,28 @@ import '../stylesheets/Post.css';
 
 const Post = () => {
   const { id } = useParams();
+  const [post, setPost] = useState({});
+  const location = useLocation();
+  const author = location.state;
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/posts/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      setPost(data.post);
+    });
+
+    // Checks for infinite rendering; delete before deployment
+    console.log('infinite post check');
+  }, [author, id]);
 
   return (
     <div className='blog-post'>
       <Nav />
-      <div className='post'>{ id }</div>
+      <div className='post'>
+        <div className="post-title">{post.title}</div>
+        <div className="post-author">{author}</div>
+      </div>
       <Footer />
     </div>
   );
