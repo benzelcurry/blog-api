58af24eebf00050710c9ff652f34eaac2acfe3f7
benzelcurry <benawhiting@gmail.com>
2023-01-12 12:24:33 -0800

    Add buttons/links to navbar

--- admin/src/components/Nav.js
+++ admin/src/components/Nav.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import '../stylesheets/Nav.css';
 
 const Nav = () => {
+  const [user, setUser] = useState();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(
+      'http://localhost:3001/',
+      { withCredentials: true },
+    )
+    .then((response) => {
+      // REMOVE CONSOLE.LOG BEFORE DEPLOYMENT
+      console.log(response.data);
+      setUser(response.data.username);
+    })
+  }, [])
+
+  const handleClick = (e) => {
+    axios.get(
+      '/logout/',
+      { withCredentials: true },
+    )
+    .then((response) => {
+      navigate(0);
+    })
+  }
+
   return (
     <div className="header">
-      <div className="site-title">Ben's Blog</div>
+      <div className="buttons">
+        <Link to={'/'}>
+          <button className="site-title">Ben's Blog</button>
+        </Link>
+        {
+          !user ?
+          <Link to={'/login'}>
+            <button className='log-in'>Log In</button>
+          </Link>
+          :
+          null
+        }
+        {
+          user ? 
+          <div className="user-buttons">
+            <button>{user}</button>
+            <button onClick={() => handleClick()}>Log Out</button>
+          </div>
+          :
+          null
+        }
+      </div>
     </div>
   );
 };
