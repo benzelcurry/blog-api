cc58015306f1520960df69de8fda1509608afc98
benzelcurry <benawhiting@gmail.com>
2023-01-12 12:33:51 -0800

    Add functional log-in form

---
+++ admin/src/components/LogIn.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
+
+import Nav from './Nav';
+import Footer from './Footer';
+import '../stylesheets/LogIn.css';
+
+const LogIn = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState();
+
+  const getToken = async (e) => {
+    e.preventDefault();
+    if (error) { setError('') }
+    if (!username) {
+      return setError('Please enter a username');
+    }
+    if (!password) {
+      return setError('Please enter a password');
+    }
+    const body = { username: username, password: password };
+    axios.post('/login', body)
+      .then((response) => {
+        if (response.data.message === 'Successful') {
+          // REMOVE CONSOLE.LOG BEFORE DEPLOYMENT
+          console.log(response);
+          setSuccess(true);
+        } else {
+          setError(response.data.error);
+        }
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  };
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="login-container">
+      <Nav />
+      {
+        success ? 
+        <Navigate to='/' />
+        : null
+      }
+      <div className="login-body">
+        <form action="http://localhost:3001/login" method="POST" className='login-form'>
+          <div className="form-group">
+            <label htmlFor="username">Username: </label>
+            <input type="text" placeholder="Username" id='username' name='username'
+              onChange={(e) => handleUsername(e)} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input type="password" placeholder="Password" id='password' name='password'
+              onChange={(e) => handlePassword(e)} />
+          </div>
+          <button onClick={(e) => getToken(e)}>Submit</button>
+        </form>
+        { error ?
+          <p className='login-error'>{error}</p>
+          : null
+        }
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default LogIn;
--- admin/src/components/RouteSwitch.js
+++ admin/src/components/RouteSwitch.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 import App from './App';
+import LogIn from './LogIn';
 
 const RouteSwitch = () => {
+  // useEffect(() => {
+  //   axios.get(
+  //     'http://localhost:3001/',
+  //     { withCredentials: true },
+  //   )
+  //   .then((response) => {
+  //     // REMOVE CONSOLE.LOG BEFORE DEPLOYMENT
+  //     console.log(response.data);
+  //     setUser(response.data.username);
+  //   })
+  // }, [])
+
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={ <App /> } />
+        <Route path='/login' element={ <LogIn /> } />
       </Routes>
     </HashRouter>
   );
