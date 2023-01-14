cf1a590c4c01d9e27b3628e9d00699d2020a593b
Ben <benawhiting@gmail.com>
2023-01-13 16:01:38 -0800

    Create and style new post form

--- admin/src/components/NewPost.js
+++ admin/src/components/NewPost.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Nav from './Nav';
 import Footer from './Footer';
 import '../stylesheets/NewPost.css';
+import { isContentEditable } from '@testing-library/user-event/dist/utils';
 
 const NewPost = () => {
+  const [content, setContent] = useState('');
+
+  const handleTyping = (e) => {
+    setContent(e.target.value);
+  };
+
   return (
     <div className="new-post-container">
       <Nav />
       <div className="new-post">
-        Hello, New Post!
+        <h1>Write A New Post</h1>
+        <form action="http://localhost:3001/posts" method="POST">
+          <div className="post-form">
+            <label htmlFor="title">Title: </label>
+            <input type="text" name="title" id="title" placeholder="Title" />
+            <label htmlFor="content">Post Content: </label>
+            <div className="post-contents">
+              <textarea
+                placeholder='Write your post body here...'
+                name="content" id="content"
+                maxLength={10000}
+                onChange={(e) => handleTyping(e)}>
+              </textarea>
+              <i>{10000 - content.length} characters remaining</i>
+            </div>
+          </div>
+          <button className='create-btn'>Create Post</button>
+        </form>
       </div>
       <Footer />
     </div>
