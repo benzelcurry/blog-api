9389a7e6efcbd143edcc2d152466c8c9b6a1327f
Ben <benawhiting@gmail.com>
2023-01-16 12:01:38 -0800

    Create and style sign-up form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -9,7 +9,17 @@ const SignUp = () => {
     <div className="signup">
       <Nav />
       <div className="signup-container">
-        Stuff goes here.
+        <form action="http://localhost:3001/users" method="POST" className='signup-form'>
+          <div className="form-inputs">
+            <label htmlFor="username">Username: </label>
+            <input type="text" name="username" id="username" placeholder="Username" />
+            <label htmlFor="password">Password: </label>
+            <input type="password" name="password" id="password" placeholder="Password" />
+            <label htmlFor="confirm_password">Confirm Password: </label>
+            <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" />
+          </div>
+          <button className='signup-btn'>Submit</button>
+        </form>
       </div>
       <Footer />
     </div>
