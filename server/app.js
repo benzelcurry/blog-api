f37b156dec285009a81cfd1ceeead3fe4716d31f
benzelcurry <benawhiting@gmail.com>
2023-01-18 13:15:31 -0800

    Add more CORS middleware

--- server/app.js
+++ server/app.js
@@ -24,6 +24,16 @@ db.on('error', console.error.bind(console, 'mongo connection error'));
 const app = express();
 
 app.use(cors({ credentials: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 app.use(helmet());
 app.use(compression());
 app.use(cookieParser());
