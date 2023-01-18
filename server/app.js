b2fc4abf64745a3bfeaf5f092214aec1b4862024
benzelcurry <benawhiting@gmail.com>
2023-01-18 13:54:34 -0800

    Update CORS

--- server/app.js
+++ server/app.js
@@ -26,7 +26,6 @@ const app = express();
 app.use(cors({ credentials: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.header('Origin'));
-  res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -37,12 +36,12 @@ app.use(function(req, res, next) {
 app.use(helmet());
 app.use(compression());
 app.use(cookieParser());
-app.use(jwt({
-  secret: process.env.SECRET_KEY,
-  algorithms: ['HS256'],
-  getToken: req => req.cookies.token
-}));
-app.use(verifyToken);
+// app.use(jwt({
+//   secret: process.env.SECRET_KEY,
+//   algorithms: ['HS256'],
+//   getToken: req => req.cookies.token
+// }));
+// app.use(verifyToken);
 app.use(express.urlencoded({ extended: false }));
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
