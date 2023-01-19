0110707737d43c425d4b962e92a7aac01ddcefb6
benzelcurry <benawhiting@gmail.com>
2023-01-19 14:42:03 -0800

    Fix bug where comments displayed wrong author

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
+import axios from 'axios';
 
 import '../stylesheets/Comment.css';
 
-const Comment = ({ comment, author }) => {
+const Comment = ({ comment }) => {
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/users`)
+    .then((response) => {
+      const users = response.data.user_list;
+      const commentAuthor = users.find(user => user._id === comment.author);
+      setAuthor(commentAuthor.username);
+    })
+  })
+
   return (
     <div className="comment-container">
       <div className="comment-header">
-        <div className="comment-author">{ author }</div>
+        <div className="comment-author">{author}</div>
         <div className="comment-date">
           {DateTime.fromISO(comment.date_posted).toLocaleString(DateTime.DATE_MED)}
         </div>
